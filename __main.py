8fe5f0c046566c00e7875c14ff581605bc3cf1e7
ro0opf <34717634+ro0opf@users.noreply.github.com>
2019-08-02 13:31:29 +0900

    Update __main.py

--- __main.py
+++ __main.py
@@ -8,6 +8,8 @@ AXES_COLOR = 'lightgoldenrodyellow'
 TOTAL_DOTS = 0
 RED_DOTS = 0
 EST_PI = float(0)
+PLOT_SIZE = 10000
+
 
 def create_button(name, pos, color, hover_color='0.975'):
     axes = plt.axes(pos)
@@ -15,17 +17,24 @@ def create_button(name, pos, color, hover_color='0.975'):
 
 
 def update_next(val, axs):
-    x = np.random.rand(100)
-    y = np.random.rand(100)
+    x = np.random.rand(PLOT_SIZE)
+    y = np.random.rand(PLOT_SIZE)
 
     global TOTAL_DOTS, RED_DOTS, EST_PI
-    TOTAL_DOTS += 100
+    TOTAL_DOTS += PLOT_SIZE
     RED_DOTS += len([i for i in (x ** 2 + y ** 2) if i <= 1])
     EST_PI = RED_DOTS / TOTAL_DOTS * 4
     axs.set_title(f'N = {TOTAL_DOTS}, PI = {EST_PI}')
 
-    col = np.where(x**2 + y ** 2 <= 1, 'r', 'k')
-    axs.scatter(x, y, s=1, c=col)
+    col = np.where(x**2 + y**2 <= 1, 'r', 'k')
+    # for i in range(1000):
+    #     if x[i] ** 2 + y[i] ** 2 <= 1:
+    #         col = 'red'
+    #     else:
+    #         col = 'black'
+    #     axs.plot(x[i], y[i],'o', ms=1, color=col)
+    #     # axs.scatter(x[i], y[i], s=1, c=col)
+    axs.scatter(x, y, s = 1, c = col)
     fig.canvas.draw_idle()
 
 
@@ -35,6 +44,7 @@ if __name__ == "__main__":
     plt.xlim(0, 1)
     plt.ylim(0, 1)
     axs.set_title(f'N = {TOTAL_DOTS}, PI = {EST_PI}')
+
     # Button init
     btn_next = create_button('Next', [0.8, 0.065, 0.1, 0.04], AXES_COLOR)
 
